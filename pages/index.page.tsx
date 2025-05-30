a98f23cff36d551c43b9459ad5df7fd05c6217c6
Dian-nur-wahid <diannur.intern@gmail.com>
2025-05-31 02:29:30 +0700

    v3

--- pages/index.page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { PageContextBuiltIn } from 'vite-plugin-ssr/types';
-import App from '../src/App';
-
-export { Page };
-
-function Page(pageContext: PageContextBuiltIn) {
-  return <App />;
-}
