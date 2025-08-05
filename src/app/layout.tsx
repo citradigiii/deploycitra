ee1ef228bbd40ef6742965ca1242b341ffd01ca0
Dian-nur-wahid <diannur.intern@gmail.com>
2025-08-05 22:40:36 +0700

    perbaikan tag google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Ads (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17291988819"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17291988819');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <HelmetProvider>
           <div className="min-h-screen flex flex-col bg-white">
